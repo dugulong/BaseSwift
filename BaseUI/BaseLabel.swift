c9f77604ff6041964c4fd98cfca5c582c21da328
Your Name <you@example.com>
2015-06-30 19:31:00 +0800

    commit label

---
+++ BaseUI/BaseLabel.swift
@@ -0,0 +1,13 @@
+//
+//  BaseLabel.swift
+//  BaseUI
+//
+//  Created by long on 15/6/30.
+//  Copyright (c) 2015年 long. All rights reserved.
+//
+
+import UIKit
+
+class BaseLabel: NSObject {
+   
+}
