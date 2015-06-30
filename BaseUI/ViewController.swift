49b07a1f403c7a5f3d303b0a50407ca2228017a2
Your Name <you@example.com>
2015-06-30 19:09:06 +0800

    Initial Commit

---
+++ BaseUI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  BaseUI
+//
+//  Created by long on 15/6/30.
+//  Copyright (c) 2015年 long. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
