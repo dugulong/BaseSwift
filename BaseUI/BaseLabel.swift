61c1bbb615b2d90076d57a293fddfc5ed39f4b45
张龙龙 <zhanglonglong@admaster.com.cn>
2015-07-01 15:58:56 +0800

    label func

--- BaseUI/BaseLabel.swift
+++ BaseUI/BaseLabel.swift
@@ -9,13 +9,29 @@
 import UIKit
 
 class BaseLabel: NSObject {
-    func initNewLabel(superView:UIView?){
-        print("baseLabel!!!");
+    
+    //初始化一个label
+    func initNewLabel(superView:UIView?,tag:Int!){
+        println("baseLabel!!!");
         let label = UILabel();
         label.frame = CGRectMake(10,50, 100, 30);
         label.backgroundColor = UIColor.grayColor();
         label.textAlignment = NSTextAlignment.Center;
         label.text = "baseLabel";
+        label.tag = tag;
+        label.textColor = UIColor.greenColor();
         superView?.addSubview(label);
+    }
+    
+    //获取一个label
+    func getBaseLabel(superView:UIView!,tag:Int!)->UILabel?{
+        var label = superView.viewWithTag(tag) as? UILabel;
+        return label;
+    }
+    
+    func getTextFont(superView:UIView!,tag:Int!)->(label:UILabel?,font:UIFont?){
+        var label = superView.viewWithTag(tag) as? UILabel;
+        var font = label?.font;
+        return (label,font)
     }
 }
--- BaseUI/ViewController.swift
+++ BaseUI/ViewController.swift
@@ -14,9 +14,26 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         let baseLabel = BaseLabel();
-        baseLabel.initNewLabel(self.view);
+        baseLabel.initNewLabel(self.view,tag:110);
+        
+        
+        var label:UILabel! = baseLabel.getBaseLabel(self.view, tag: 110);
+        if label != nil {
+            label.text = "new text!!";
+        }else{
+           println("label is nil!!")
+        }
+        
+        var list = baseLabel.getTextFont(self.view, tag: 110);
+        println("label text:\(list.label?.text),fontname:\(list.font?.fontName)")
     }
+    
 
+    override func touchesBegan(touches: Set<NSObject>, withEvent event: UIEvent) {
+         var label = self.view.viewWithTag(110) as? UILabel;
+         label?.removeFromSuperview();
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
